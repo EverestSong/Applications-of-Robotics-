4715b05073828bb540a24e68c935039a038d7233
EverestSong <9935960@schoolsnet.act.edu.au>
2026-08-24 09:11:46 +1000

    Update ultrasonic.ino

--- ultrasonic.ino
+++ ultrasonic.ino
@@ -1,39 +1,42 @@
-const int echoPin = 8;
-const int trigPin = 9;
+const int echoPin = 6;
+const int trigPin = 7;
+
+const unsigned long sensorInterval = 100;
+unsigned long previousSensorTime = 0;
 
 void setup() {
   pinMode(echoPin, INPUT);
   pinMode(trigPin, OUTPUT);
-
   Serial.begin(9600);
 }
 
 void loop() {
-  // duration: Duration of ping (μs)
-  // distance: The distance (cm)
-  long duration, distance;
+  unsigned long currentSensorTime = millis();
 
-  // The PING is triggered by a HIGH pulse of 2 or more μs.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  if (currentSensorTime - previousSensorTime >= sensorInterval) {
+    float distance = ultrasonicSensorDistance();
+    Serial.print(distance);
+    Serial.print(" cm");
+    Serial.println();  
+    previousSensorTime = currentSensorTime;
+  }
+}
 
+float ultrasonicSensorDistance() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(5);
   digitalWrite(trigPin, LOW);
 
-  duration = pulseIn(echoPin, HIGH);
+  float duration = pulseIn(echoPin, HIGH);
+  float distance = microsecondsToCentimeters(duration);
 
-  // Convert the time into a distance
-  distance = microsecondsToCentimeters(duration);
-  
-  Serial.print(distance);
-  Serial.print(" cm");
-  Serial.println();  
-  delay(100);
+  return distance;
 }
 
-long microsecondsToCentimeters(long microseconds)
+float microsecondsToCentimeters(float microseconds)
 {
-  return microseconds / 29 / 2;
+  // Since distance = (speed * time) / 2 
+  return microseconds * 0.0343 / 2;
 }
